779f1a5444c9c44d694d137711b79b6c280bd90e
MarkMa512 <42110231+MarkMa512@users.noreply.github.com>
2022-07-31 15:16:33 +0800

    working on rehash crl

--- broker/rehash_crl.sh
+++ broker/rehash_crl.sh
@@ -1,3 +1,8 @@
 cd rabbit-1
 # This does not work for crl chain file 
 # openssl rehash crl
+
+for file in *.pem; do ln -s "$file" "$(openssl crl -hash -noout -in "$file")".0; done
+
+# does not work on RHEL; no output
+# c_rehash crl 
